19daa0ba0e1182581b5130a4013c982311532233
Ilia Kempi <ilia.kempi@aalto.fi>
2019-09-16 17:22:17 +0300

    Added new blackbox (which compiles)

--- src/main/scala/async_set_register/async_set_register.scala
+++ src/main/scala/async_set_register/async_set_register.scala
@@ -37,6 +37,31 @@ setInline("async_set_register.v",
 
 }
 
+setInline("async_set_register.v",
+    s"""
+    |module async_set_register #( n=8 ) (
+    |    input  [n-1:0] D,
+    |    output  [n-1:0] Q,
+    |    input clock,
+    |    input set
+    |);
+    |reg [n-1:0] _Q;
+    |always @(posedge clock or posedge set) begin
+    |    if(set == 1) 
+    |    begin
+    |      _Q <= {n{1'b1}};
+    |    end 
+    |    else 
+    |    begin
+    |      _Q <= D;
+    |    end
+    |end
+    |assign Q = _Q;
+    |endmodule
+    """.stripMargin)
+
+}
+
 class async_set_register_inst(val n : Int=8 ) extends Module {
         val io = IO(new Bundle { 
             val D = Input(UInt(n.W))
